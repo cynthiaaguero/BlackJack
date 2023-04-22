a598c503556e253a8ea557d145534455334402cd
cynthiaaguero <cynthiaaguero02@gmail.com>
2023-04-22 15:18:11 -0400

    we starting this up fr

---
+++ blackjack.js
@@ -0,0 +1,46 @@
+
+var dealerSum = 0;
+var yourSum = 0;
+
+var dealerAceCount = 0;
+var yourAceCount = 0; 
+
+var hidden; //keep track of dealers hidden card
+var deck;
+
+var canHit; //allows the player to draw 
+
+window.onload = function(){
+    buildDeck();
+    shuffleDeck();
+    startGame();
+}
+
+function buildDeck(){
+    let values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K",];
+    let types = ["C", "D", "H", "S",];
+    
+    deck = [];
+
+    for(let i = 0; i < types.length; i++){
+        for(let j = 0; j < values.length; j++){
+            deck.push(values[j] + "-" + types[i]); //grabs cards by image
+        }
+    }
+    console.log(deck);
+}
+
+function shuffleDeck(){
+    for(let i = 0; i < deck.length; i++){
+        let j = Math.floor(Math.random()*deck.length); //(0-1) * 52 = (0-51.999)
+        let temp = deck[i];
+        deck[i] = deck[j];
+        deck[j] = temp;
+    }
+    console.log(deck);
+}
+
+function startGame(){
+    hidden = deck.pop();
+    dealerSum += getValue(hidden);
+}
